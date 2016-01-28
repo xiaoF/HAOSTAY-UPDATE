7b8942dae4a934edf9558455fb0d87021a6cad2a
xiaoF <870501@qq.com>
2016-01-28 21:57:28 +0800

    commit

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# HAOSTAY-UPDATE
+
+**SaviDB**
+-------
+
+> Native data store for Cordova。
+
+
+1. SaviDB is base on SharePreference @Android
+2. SaviDB is base on NSUserDefaults @iOS
+
+
+> Javascript API
+
+ 1. SaviDB.get(key, successCallback, errorCallback);
+ 2. SaviDB.set(key,value, successCallback, errorCallback);
+ 3. SaviDB.remove(key, successCallback, errorCallback);
+ 4. SaviDB.clear( successCallback, errorCallback);
---
+++ src/android/CheckUpdatePlugin.java
@@ -0,0 +1,63 @@
+package com.haostay.mobile.book;
+
+
+import android.content.SharedPreferences;
+import android.content.pm.PackageInfo;
+import android.util.Log;
+
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+
+public class CheckUpdatePlugin extends CordovaPlugin {
+
+	 @Override
+	  public boolean execute(String action, JSONArray optionsArr, CallbackContext callbackContext) throws JSONException{
+
+		 if (action.equals("checkVersion")) {
+	        	Log.i("out","checkVersion");
+	        	boolean flag=updateVersion();
+	        	 this.checkVersion(flag,callbackContext);
+	            return flag;
+	        }
+	        return false;
+	    }
+	 
+	
+
+	private void checkVersion(boolean flag,CallbackContext callbackContext) {
+		System.out.println("NewVersion--------------------------");
+		Log.i("out","NewVersion");
+	        if (flag) {
+	            callbackContext.success("New Version");
+	        } else {
+	            callbackContext.error("Same Version.");
+	        }
+	    }
+	
+	 private Boolean  updateVersion() {
+		 Log.i("out", "updateVersion");
+		 boolean flag=false;
+		 String versionName=null;
+		 try {
+			 versionName = cordova.getActivity().getPackageManager().getPackageInfo(cordova.getActivity().getPackageName(), 0).versionName;
+		 }catch (Exception e){
+
+		 }
+
+
+		Update ap = new Update(cordova.getActivity());
+		Log.i("out","Update");
+		if (ap.getServerVer(preferences.getString("AndroidUpdateUrl", null),versionName)) {
+			Log.i("out","getServerVer");
+				ap.doNewVersionUpdate();// 更新版本
+				flag= true;
+		}else{
+			flag= false;
+		}
+		return flag;
+	}
+
+}
